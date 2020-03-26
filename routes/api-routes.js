a18ffbf315b427769625cdc12ed1e09404b7a23e
Jordan Coughenour <jcoughenour18@gmail.com>
2020-03-26 08:58:58 -0400

    updating my api-routes. still no connection for exercises

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,10 +12,9 @@ module.exports = function(app) {
     })
 
     app.put("/api/workouts/:id", (req, res) => {
-        db.Workout.findByIdAndUpdate({_id : req.params.id}, {$push: {exercises: req.body}})
-        .then(addedExercise => {
-            res.json(addedExercise)
-        })
+        db.Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}}
+            {runValidators:true, new: true})
+        .then(results => res.json(results))
         .catch(err => {
             console.log(err);
         })
