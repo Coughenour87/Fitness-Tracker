4007a5487ace119379154e072c2e6ce244871f0a
Jordan Coughenour <jcoughenour18@gmail.com>
2020-03-26 09:45:55 -0400

    updated formatting

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -13,8 +13,13 @@ module.exports = function(app) {
 
     app.put("/api/workouts/:id", (req, res) => {
         db.Workout.findByIdAndUpdate (
-            req.params.id, {$push: {exercises: req.body}},
-            {runValidators:true, new: true})
+            req.params.id, 
+            {
+                $push: {exercises: req.body}
+            },
+            {
+                runValidators:true, new: true
+            })
         .then(results => res.json(results))
         .catch(err => {
             console.log(err);
